e7f3b9ec8a49de87c964f94fcb9d1685ba31048c
JamesPajunar <kaepajunar@gmail.com>
2022-05-12 14:28:11 +0800

    First commit

---
+++ Jump Game/Assets/Scripts/CharacterCamera.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCamera : MonoBehaviour
+{
+    [SerializeField] Camera camera = default;
+    public Rigidbody2D rb;
+    
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+// First Background
+        if (rb.transform.position.y >= -3.42 && rb.transform.position.y < 6f)
+        {
+            camera.transform.position = new Vector3(0.0f, 0.0f, -10.0f);
+        }
+
+        else if (rb.transform.position.y >= 6f && rb.transform.position.y < 17.5f)
+        {
+            camera.transform.position = new Vector3(0.0f, 11.7f, -10.0f);
+            
+        }
+        else if (rb.transform.position.y >= 17.5f && rb.transform.position.y < 29f)
+        {
+            camera.transform.position = new Vector3(0.0f, 23.4f, -10.0f);
+
+        }
+        else if (rb.transform.position.y >= 29f && rb.transform.position.y < 40.5f)
+        {
+            camera.transform.position = new Vector3(0.0f, 35.1f, -10.0f);
+
+        }
+        else if (rb.transform.position.y >= 40.5f && rb.transform.position.y < 52f)
+        {
+            camera.transform.position = new Vector3(0.0f, 46.8f, -10.0f);
+        }
+
+// Second Background
+        else if (rb.transform.position.y >= 52f && rb.transform.position.y < 63.5f)
+        {
+            camera.transform.position = new Vector3(0.0f, 58.5f, -10.0f);
+        }
+        else if (rb.transform.position.y >= 63.5f && rb.transform.position.y < 75f)
+        {
+            camera.transform.position = new Vector3(0.0f, 70.2f, -10.0f);
+        }
+        else if (rb.transform.position.y >= 75f && rb.transform.position.y < 86.5f)
+        {
+            camera.transform.position = new Vector3(0.0f, 81.9f, -10.0f);
+        }
+        else if (rb.transform.position.y >= 86.5f && rb.transform.position.y < 98f)
+        {
+            camera.transform.position = new Vector3(0.0f, 93.6f, -10.0f);
+        }
+        else if (rb.transform.position.y >= 98f && rb.transform.position.y < 109.5f)
+        {
+            camera.transform.position = new Vector3(0.0f, 105.3f, -10.0f);
+        }
+
+// Third Background
+        else if (rb.transform.position.y >= 109.5f && rb.transform.position.y < 121f)
+        {
+            camera.transform.position = new Vector3(0.0f, 117f, -10.0f);
+        }
+        else if (rb.transform.position.y >= 121f && rb.transform.position.y < 132.5f)
+        {
+            camera.transform.position = new Vector3(0.0f, 128.7f, -10.0f);
+        }
+        else if (rb.transform.position.y >= 132.5f && rb.transform.position.y < 144f)
+        {
+            camera.transform.position = new Vector3(0.0f, 140.4f, -10.0f);
+        }
+        else if (rb.transform.position.y >= 144f && rb.transform.position.y < 155.5f)
+        {
+            camera.transform.position = new Vector3(0.0f, 152.1f, -10.0f);
+        }
+        else if (rb.transform.position.y >= 155.5f && rb.transform.position.y < 167f)
+        {
+            camera.transform.position = new Vector3(0.0f, 163.8f, -10.0f);
+        }
+    }
+}
---
+++ Jump Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public SpriteRenderer sp;
+    private float moveInput;
+    public float walkSpeed;
+
+    public float jumpValue;
+    public bool isGrounded;
+    public bool isRightSide;
+    public bool isLeftSide;
+    public LayerMask groundMask;
+    public Transform groundCheck;
+    public Transform rightCheck;
+    public Transform leftCheck;
+
+    private float yPosition;
+
+    public PhysicsMaterial2D bounceMat, normalMat;
+
+    public Animator animator;
+
+    [Header("Events")]
+    public UnityEvent OnLandEvent;
+
+    [System.Serializable]
+    public class BoolEvent : UnityEvent<bool> { }
+    
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        if (OnLandEvent == null)
+            OnLandEvent = new UnityEvent();
+    }
+
+
+
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        moveInput = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("speed", Mathf.Abs(moveInput));
+        
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundMask);
+        isRightSide = Physics2D.OverlapCircle(rightCheck.position, 0.2f, groundMask);
+        isLeftSide = Physics2D.OverlapCircle(leftCheck.position, 0.2f, groundMask);
+
+
+        if (Input.GetKey("space") && isGrounded)
+        {
+            jumpValue += 0.09f;
+            
+        }
+
+        if (jumpValue >= 20.0f && isGrounded)
+        {
+            rb.velocity = new Vector2(moveInput * (jumpValue/3), jumpValue );
+            
+
+            Invoke("ResetJump", 0.2f);
+            OnLandEvent.Invoke();
+
+
+        }
+
+        if (Input.GetKeyDown("space") && isGrounded)
+        {
+            
+            rb.velocity = new Vector2(0.0f, rb.velocity.y);
+            animator.SetBool("isJumping", true);
+
+        }
+
+        if (jumpValue == 5.0f && isGrounded)
+        {
+            rb.velocity = new Vector2(moveInput * walkSpeed, rb.velocity.y);
+        }
+
+        if (Input.GetKeyUp("space") && isGrounded)
+        {
+            rb.velocity = new Vector2(moveInput * (jumpValue/2), jumpValue);
+            Invoke("ResetJump", 0.2f);
+            OnLandEvent.Invoke();
+            
+        }
+
+        if (isRightSide || isLeftSide)
+        {
+            rb.sharedMaterial = bounceMat;
+        }
+        else
+        {
+            rb.sharedMaterial = normalMat;
+        }
+
+        yPosition = rb.position.y - 6.0f;
+
+        
+
+
+        FlipPlayer();
+    }
+
+    public void FlipPlayer()
+    {
+        if (rb.velocity.x < -0.1f)
+        {
+            sp.flipX = true;
+        }
+        else if (rb.velocity.x > 0.1f)
+        {
+            sp.flipX = false;
+        }
+    }
+
+    void ResetJump()
+    {
+        jumpValue = 5.0f;
+    }
+
+    
+    public void OnLanding ()
+    {
+        animator.SetBool("isJumping", false);
+    }
+}
